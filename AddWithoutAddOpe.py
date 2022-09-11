6784c1b4d51a41b119a8f27818f829ca207cd50d
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-09-11 16:50:36 +0530

    Day-3 completed

---
+++ AddWithoutAddOpe.py
@@ -0,0 +1,10 @@
+//Addition of two numbers without use an addition operator
+a=(int(input("enter n1")));
+b=(int(input("enter n1")));
+c=0
+d=0
+while(a>=c):
+    d=b
+    b=b+1
+    c=c+1
+print(d)    
