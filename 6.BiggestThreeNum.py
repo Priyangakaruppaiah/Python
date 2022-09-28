c6e265e1fe03f9d4ad254728fd81c2af5992e4d2
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-09-28 21:35:20 +0530

    Day-6 completed

---
+++ 6.BiggestThreeNum.py
@@ -0,0 +1,11 @@
+//Python program to find biggest of three number
+a=int(input("enter the number1:"))
+b=int(input("enter the number2:"))
+c=int(input("enter the number3:"))
+if (a>b and a>c):
+    print("a is big")
+elif (b>a and b>c):
+    print("b is big")
+else:
+    print("c is big")
+    
