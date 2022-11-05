9005079fe4e119b9f3d2ffa83c1614c4c3c73106
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-06 00:51:48 +0530

    Day-11 completed

---
+++ 19.Calendar.py
@@ -0,0 +1,18 @@
+import calendar
+year=int(input("enter the year:"))
+month=int(input("enter the month:"))
+print(calendar.month(year,month))
+
+" " "
+  OUTPUT:
+enter the year:2000
+enter the month:10
+    October 2000
+Mo Tu We Th Fr Sa Su
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+" " "
