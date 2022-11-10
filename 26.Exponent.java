e51021d61e85c39977491f5ef8c679889aa139bd
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-10 07:02:20 +0530

    Day-18 completed

---
+++ 26.Exponent.java
@@ -0,0 +1,11 @@
+//Find the exponent
+base=int(input())
+expo=int(input())
+result=base
+if(expo!=0):
+    for i in range(expo-1):
+        result=result*base
+
+else:
+    result==1
+print(result)        
