3bf967b18f4aaa2a6359a7ada1fa47b8d16136f5
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-13 10:30:21 +0530

    Day-18 completed

---
+++ 32.WordReverse.py
@@ -0,0 +1,11 @@
+# Reverse a word in given string
+x = input("Enter any string: ")
+a = x.split()
+a.reverse()
+print(' '.join(a))
+
+'''
+OUTPUT:
+Enter any string: Welcome to python
+python to Welcome
+'''
