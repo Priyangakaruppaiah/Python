4eec11e1b34f62110138c5e74662189278501e40
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-14 18:55:20 +0530

    Day-19 completed

---
+++ 33.PrintAlpha.py
@@ -0,0 +1,11 @@
+# print the alphabets and remove other special characters and digits
+a=input()
+for i in a:
+    if i.isalpha():
+        print(i,end=" ")
+        
+ '''
+ OUTPUT:
+ fjdtr#4%jh
+f j d t r j h 
+ '''
