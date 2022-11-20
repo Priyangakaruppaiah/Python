08c018e66bc3f6c241e16a3d87d7698a25265f9a
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-20 18:18:15 +0530

    Day-26 completed

---
+++ 43.PrimeNumber.py
@@ -0,0 +1,18 @@
+n=[3,4,5,7,34,56,23,11,13,10,6,8,80]
+m=len(n)
+print(m)
+a=[]
+for i in range(0,m):
+    #c=2
+    s=1
+    for c in range(2,n[i]):
+        if(n[i]%c==0):
+            a.append(n[i]) 
+            break
+print(a)
+'''
+OUTPUT:
+13
+[4, 34, 56, 10, 6, 8, 80]
+
+'''
