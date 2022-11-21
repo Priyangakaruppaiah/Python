5d76e64371eacad0e4d0d02c906f4a481245292c
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-21 12:26:48 +0530

    Day-26 completed

---
+++ 44.PrimeFromTo.py
@@ -0,0 +1,40 @@
+# enter the prime number starting to ending
+n1=int(input())
+n2=int(input())
+for i in range(n1,n2):
+    c=0
+    for j in range(2,i):
+        if(i%j==0):
+            c=1
+    if(c==0):
+        print(i)
+    
+'''
+OUTPUT:
+3
+79
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+
+'''
+
+
+      
