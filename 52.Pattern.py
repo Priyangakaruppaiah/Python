1cfb0a146842eb2e56dfa928b68a3f696b1ace2e
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-26 06:48:15 +0530

    Create 52.Pattern.py

---
+++ 52.Pattern.py
@@ -0,0 +1,24 @@
+n=int(input())
+for i in range(0,n):
+    for j in range(0,n-i):
+       print("  ",end=" ")
+    for j in range(0,i+1):
+       print(" *  ",end=" ")
+    print("\n") 
+    
+'''
+OUTPUT:
+6
+                   *   
+
+                *    *   
+
+             *    *    *   
+
+          *    *    *    *   
+
+       *    *    *    *    *   
+
+    *    *    *    *    *    *   
+
+'''
