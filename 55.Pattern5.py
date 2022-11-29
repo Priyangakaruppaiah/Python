d168441e770e5aeba4a2f4142972b7e3b2f52a6e
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-29 20:53:08 +0530

    Day-27 completed

---
+++ 55.Pattern5.py
@@ -0,0 +1,18 @@
+# upper triangle
+n=int(input())
+for i in range(0,n):
+    for j in range(0,n):
+        if(i<=j):
+            print("*",end=" ")
+    print('\n') 
+'''
+OUTPUT:
+4
+* * * * 
+
+* * * 
+
+* * 
+
+* 
+'''
